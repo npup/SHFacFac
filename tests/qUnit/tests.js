7bd7b7dadc0bd0ea96837fe76eb6692cc87ef076
P. Envall <petter.envall@gmail.com>
2010-11-03 13:10:06 +0100

    Tabs to spaces..

--- tests/qUnit/tests.js
+++ tests/qUnit/tests.js
@@ -1,16 +1,16 @@
 (function () {
 
 function getFunctionName(f) {
-	var match = (''+f).match(/function \s*([\w\$]*)\s*\(/);
-	if (match) {return match[1];}
+    var match = (''+f).match(/function \s*([\w\$]*)\s*\(/);
+    if (match) {return match[1];}
 }
 // Model
 function Apa(name, age) {
-	this.name = name;
-	this.age = age;
+    this.name = name;
+    this.age = age;
 }
 Apa.prototype.toString = function () {
-	return 'En apa som heter '+this.name+' med åldern '+this.age+'.';
+    return 'En apa som heter '+this.name+' med åldern '+this.age+'.';
 };
 
 // setup of factory, removing items with this test-factory's prefix
@@ -18,19 +18,19 @@ var factoryName = 'qUnit-test';
 var Factory = SHFacFac(window, factoryName);
 var factoryPrefix = Factory.getPrefix();
 for (var item in localStorage) {
-	if (item.indexOf(factoryPrefix)===0) {
-		localStorage.removeItem(item);
-	}
+    if (item.indexOf(factoryPrefix)===0) {
+        localStorage.removeItem(item);
+    }
 }
 
 module('Factory');
 /*****************************************************
 * Sanity check of a created factory
 ******************************************************/
-test('Factory creation', function () {	
-	equal(typeof Factory.createStore, 'function', 'Factory has a function "createStore"');
-	equal(typeof Factory.getName, 'function', 'Factory has a function "getName"');
-	equal(Factory.getName(), factoryName, 'Name of factory works as expected');
+test('Factory creation', function () {  
+    equal(typeof Factory.createStore, 'function', 'Factory has a function "createStore"');
+    equal(typeof Factory.getName, 'function', 'Factory has a function "getName"');
+    equal(Factory.getName(), factoryName, 'Name of factory works as expected');
 });
 
 
@@ -41,27 +41,27 @@ var Apor = Factory.createStore(Apa);
 ******************************************************/
 test('Store API', function () {
 
-	var propTypes = {
-		store: 'function'
-		, retrieve: 'function'
-		, remove: 'function'
-		, list: 'function'
-		, count: 'function'
-		, clear: 'function'
-		, info: 'function'
-		, _internal: 'object'
-	}, item, actualType, expectedType;
-	
-	for (item in Apor) {
-		actualType = typeof Apor[item];
-		expectedType = propTypes[item];
-		if (typeof expectedType=='undefined') {
-			ok(false, 'Unknown property "'+item+'" (type '+actualType+'), remove or update tests!');
-		}
-		else {
-			equal(actualType, expectedType, 'Prop "'+item+'" is of expected type: '+propTypes[item]);
-		}
-	}
+    var propTypes = {
+        store: 'function'
+        , retrieve: 'function'
+        , remove: 'function'
+        , list: 'function'
+        , count: 'function'
+        , clear: 'function'
+        , info: 'function'
+        , _internal: 'object'
+    }, item, actualType, expectedType;
+    
+    for (item in Apor) {
+        actualType = typeof Apor[item];
+        expectedType = propTypes[item];
+        if (typeof expectedType=='undefined') {
+            ok(false, 'Unknown property "'+item+'" (type '+actualType+'), remove or update tests!');
+        }
+        else {
+            equal(actualType, expectedType, 'Prop "'+item+'" is of expected type: '+propTypes[item]);
+        }
+    }
 });
 
 
@@ -69,39 +69,39 @@ test('Store API', function () {
 * Sanity check of the internal props of a created store
 ******************************************************/
 test('Store, internal props', function() {
-	var internal = Apor._internal, internalPropTypes = {
-		clazz: 'function'
-		, clazzName: 'string'
-		, Storage: 'object'
-		, NS: 'string'
-		, PREFIX: 'string'
-		, prop: 'object'
-	}, item, actualType, expectedType;
-	
-	for (item in internal) {
-		actualType = typeof internal[item];
-		expectedType = internalPropTypes[item];
-		if (typeof expectedType=='undefined') {
-			ok(false, 'Unknown property "'+item+'" (type '+actualType+'), remove or update tests!');
-		}
-		else {
-			equal(actualType, expectedType, 'Internal prop "'+item+'" is of expected type: '+internalPropTypes[item]);
-		}
-	}
+    var internal = Apor._internal, internalPropTypes = {
+        clazz: 'function'
+        , clazzName: 'string'
+        , Storage: 'object'
+        , NS: 'string'
+        , PREFIX: 'string'
+        , prop: 'object'
+    }, item, actualType, expectedType;
+    
+    for (item in internal) {
+        actualType = typeof internal[item];
+        expectedType = internalPropTypes[item];
+        if (typeof expectedType=='undefined') {
+            ok(false, 'Unknown property "'+item+'" (type '+actualType+'), remove or update tests!');
+        }
+        else {
+            equal(actualType, expectedType, 'Internal prop "'+item+'" is of expected type: '+internalPropTypes[item]);
+        }
+    }
 });
 
 /*****************************************************
 * Store basic functionality
 ******************************************************/
 test('Store functionality', function () {
-	equal(Apor.count(), 0, 'Store is initially empty');
-	var ola = new Apa('Ola', 28);
-	var olaId = Apor.store(ola);
-	equal(typeof olaId, 'string', 'Storing an object returns a string id');
-	equal(Apor.count(), 1, 'After storing one object in an empty store, count is 1');
-	var ola2 = Apor.retrieve(olaId);
-	ok(ola2!==null && typeof ola2=='object', 'Retrieved a defined object');
-	equal(getFunctionName(ola2.constructor), getFunctionName(Apa), 'Retrieved object was of correct instance type');
+    equal(Apor.count(), 0, 'Store is initially empty');
+    var ola = new Apa('Ola', 28);
+    var olaId = Apor.store(ola);
+    equal(typeof olaId, 'string', 'Storing an object returns a string id');
+    equal(Apor.count(), 1, 'After storing one object in an empty store, count is 1');
+    var ola2 = Apor.retrieve(olaId);
+    ok(ola2!==null && typeof ola2=='object', 'Retrieved a defined object');
+    equal(getFunctionName(ola2.constructor), getFunctionName(Apa), 'Retrieved object was of correct instance type');
 });
 
 })();
